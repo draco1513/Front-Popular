9f26bd97414477cfc8fddb6b5f4ee6ea948a7930
dm <dmendoza@popular-safi.com>
2023-06-15 17:51:01 -0500

    a

--- src/app/views/sessions/signin2/signin2.component.ts
+++ src/app/views/sessions/signin2/signin2.component.ts
@@ -1,5 +1,16 @@
-import { Component, OnInit, ViewChild, OnDestroy, AfterViewInit } from '@angular/core';
-import { UntypedFormGroup, UntypedFormBuilder, UntypedFormControl, Validators } from '@angular/forms';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  OnDestroy,
+  AfterViewInit,
+} from '@angular/core';
+import {
+  UntypedFormGroup,
+  UntypedFormBuilder,
+  UntypedFormControl,
+  Validators,
+} from '@angular/forms';
 import { MatButton } from '@angular/material/button';
 import { MatProgressBar } from '@angular/material/progress-bar';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -10,7 +21,7 @@ import { Subject } from 'rxjs/internal/Subject';
 @Component({
   selector: 'app-signin2',
   templateUrl: './signin2.component.html',
-  styleUrls: ['./signin2.component.scss']
+  styleUrls: ['./signin2.component.scss'],
 })
 export class Signin2Component implements OnInit, AfterViewInit, OnDestroy {
   @ViewChild(MatProgressBar) progressBar: MatProgressBar;
@@ -21,25 +32,24 @@ export class Signin2Component implements OnInit, AfterViewInit, OnDestroy {
 
   private _unsubscribeAll: Subject<any>;
 
-
-  constructor(private fb: UntypedFormBuilder,
+  constructor(
+    private fb: UntypedFormBuilder,
     private jwtAuth: JwtAuthService,
     private egretLoader: AppLoaderService,
     private router: Router,
-    private route: ActivatedRoute) {
+    private route: ActivatedRoute
+  ) {
     this._unsubscribeAll = new Subject();
   }
 
   ngOnInit() {
-
     const password = new UntypedFormControl('', Validators.required);
 
     this.signinForm = new UntypedFormGroup({
       username: new UntypedFormControl('Watson', Validators.required),
       password: new UntypedFormControl('12345678', Validators.required),
-      rememberMe: new UntypedFormControl(true)
-    }
-    );
+      rememberMe: new UntypedFormControl(true),
+    });
   }
 
   onSubmit() {
@@ -58,34 +68,40 @@ export class Signin2Component implements OnInit, AfterViewInit, OnDestroy {
   }
 
   signin() {
-    const signinData = this.signinForm.value
+    const signinData = this.signinForm.value;
 
     this.submitButton.disabled = true;
     this.progressBar.mode = 'indeterminate';
 
-    this.jwtAuth.signin(signinData.username, signinData.password)
-      .subscribe(response => {
+    this.jwtAuth.signin(signinData.username, signinData.password).subscribe(
+      (response) => {
         this.router.navigateByUrl(this.jwtAuth.return);
-      }, err => {
+      },
+      (err) => {
         this.submitButton.disabled = false;
         this.progressBar.mode = 'determinate';
         this.errorMsg = err.message;
+
         // console.log(err);
-      })
+      }
+    );
   }
 
   autoSignIn() {
     if (this.jwtAuth.return === '/') {
-      return
+      return;
     }
-    this.egretLoader.open(`Automatically Signing you in! \n Return url: ${this.jwtAuth.return.substring(0, 20)}...`, { width: '320px' });
+    this.egretLoader.open(
+      `Automatically Signing you in! \n Return url: ${this.jwtAuth.return.substring(
+        0,
+        20
+      )}...`,
+      { width: '320px' }
+    );
     setTimeout(() => {
       this.signin();
       console.log('autoSignIn');
-      this.egretLoader.close()
+      this.egretLoader.close();
     }, 2000);
   }
-
-
-
 }
